19c83b4f10bc15898be6c3985cb5dd0afc87618c
Peiwen Lu <hi@peiwen.lu>
2018-01-28 13:01:23 +0800

    Convert double quotes to single

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-config-recommended-scss"],
-  plugins: ["stylelint-order", "stylelint-scss"],
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss'],
+  plugins: ['stylelint-order', 'stylelint-scss'],
   rules: {
     // override stylelint-config-standard rules
-    "at-rule-empty-line-before": [
-      "always",
+    'at-rule-empty-line-before': [
+      'always',
       {
-        except: ["blockless-after-same-name-blockless", "first-nested"],
-        ignore: ["after-comment"],
-        ignoreAtRules: ["else"]
-      }
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment'],
+        ignoreAtRules: ['else'],
+      },
     ],
-    "block-closing-brace-newline-after": [
-      "always",
+    'block-closing-brace-newline-after': [
+      'always',
       {
-        ignoreAtRules: ["if", "else"]
-      }
+        ignoreAtRules: ['if', 'else'],
+      },
     ],
 
     // scss syntax rules
-    "scss/at-else-empty-line-before": "never",
-    "scss/at-extend-no-missing-placeholder": true,
-    "scss/at-import-no-partial-leading-underscore": true,
-    "scss/dollar-variable-colon-newline-after": "always-multi-line",
-    "scss/dollar-variable-colon-space-after": "always-single-line",
-    "scss/dollar-variable-colon-space-before": "never",
-    "scss/dollar-variable-empty-line-before": [
-      "always",
+    'scss/at-else-empty-line-before': 'never',
+    'scss/at-extend-no-missing-placeholder': true,
+    'scss/at-import-no-partial-leading-underscore': true,
+    'scss/dollar-variable-colon-newline-after': 'always-multi-line',
+    'scss/dollar-variable-colon-space-after': 'always-single-line',
+    'scss/dollar-variable-colon-space-before': 'never',
+    'scss/dollar-variable-empty-line-before': [
+      'always',
       {
-        except: ["after-dollar-variable", "first-nested"],
-        ignore: ["after-comment"]
-      }
+        except: ['after-dollar-variable', 'first-nested'],
+        ignore: ['after-comment'],
+      },
     ],
-    "scss/dollar-variable-no-missing-interpolation": true,
-    "scss/double-slash-comment-empty-line-before": [
-      "always",
+    'scss/dollar-variable-no-missing-interpolation': true,
+    'scss/double-slash-comment-empty-line-before': [
+      'always',
       {
-        except: ["first-nested"],
-        ignore: ["between-comments", "stylelint-commands"]
-      }
+        except: ['first-nested'],
+        ignore: ['between-comments', 'stylelint-commands'],
+      },
     ],
-    "scss/double-slash-comment-whitespace-inside": "always",
-    "scss/declaration-nested-properties": "never",
-    "scss/operator-no-unspaced": true,
-    "scss/selector-no-redundant-nesting-selector": true,
+    'scss/double-slash-comment-whitespace-inside': 'always',
+    'scss/declaration-nested-properties': 'never',
+    'scss/operator-no-unspaced': true,
+    'scss/selector-no-redundant-nesting-selector': true,
 
     // order rules
-    "order/order": [
-      "custom-properties",
-      "dollar-variables",
+    'order/order': [
+      'custom-properties',
+      'dollar-variables',
       {
-        type: "at-rule",
-        name: "extend"
+        type: 'at-rule',
+        name: 'extend',
       },
       {
-        type: "at-rule",
-        name: "include"
+        type: 'at-rule',
+        name: 'include',
       },
-      "declarations",
-      "at-rules",
-      "rules",
+      'declarations',
+      'at-rules',
+      'rules',
       {
-        type: "at-rule",
-        name: "media"
-      }
-    ]
-  }
+        type: 'at-rule',
+        name: 'media',
+      },
+    ],
+  },
 };
